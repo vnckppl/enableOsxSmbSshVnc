b1605af21acecd5b202bebe7368d5264928a03e1
Vincent Koppelmans <vincent.koppelmans@utah.edu>
2019-03-07 14:31:03 -0700

    Removed links, removed VNC because remote managament includes VNC

--- startSharing.sh
+++ startSharing.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# 2019-03-05
-
-# Enable SSH - SMB - VNC - Remote Management
+# Enable SSH - SMB - VNC - Remote Management in OSX
 
 # SSH
-# http://osxdaily.com/2016/08/16/enable-ssh-mac-command-line/
 echo $(tput setaf 1)"SSH:"$(tput sgr0)
 if [ "$(sudo systemsetup -getremotelogin | grep Off)" ]; then
     echo "Remote login via SSH is disabled"
@@ -20,7 +17,6 @@ fi
 
 
 # SMB
-# https://apple.stackexchange.com/questions/136649/enable-smb-sharing-from-command-line
 echo $(tput setaf 1)"SMB:"$(tput sgr0)
 if [ "$(sudo launchctl list | grep smbd)" ]; then
     echo "File sharing via SMB is enabled"
@@ -33,23 +29,7 @@ else \
     sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server.plist EnabledServices -array disk
 fi
 
-
-# VNC
-# https://www.techrepublic.com/article/how-to-enable-screen-sharing-on-macs-via-terminal/
-# https://apple.stackexchange.com/questions/200549/enable-screensharing-via-ssh-and-setting-password
-# echo $(tput setaf 1)"VNC:"$(tput sgr0)
-# if [ "$(sudo launchctl list | grep -i screensharing)" ]; then
-#     echo "Screen sharing via VNC is enabled"
-#     echo "Disabling screen sharing via VNC"
-#     sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
-# else \
-#     echo "Screen sharing via VNC is disabled"
-#     echo "Enabling screen sharing via VNC"
-#     sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
-# fi
-
 # Remote Management
-# https://apple.stackexchange.com/questions/30238/how-to-enable-os-x-screen-sharing-vnc-through-ssh
 echo $(tput setaf 1)"Remote management:"$(tput sgr0)
 ks="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
 if [ "$(sudo launchctl list | grep -i screensharing)" ]; then
